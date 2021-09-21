5e23256e0de79729513fc792f20ac7917e09b028
Defendi <alexandre_defendi@hotmail.com>
2021-09-21 16:55:07 -0300

    arquivo do config.php

---
+++ tests/config.php
@@ -0,0 +1,17 @@
+<?php
+    $tatico = new StdClass();
+	$tatico->usuario = 'twodogs.api';
+	$tatico->senha = 'Jc]YwC~j//93';
+	$tatico->cn = 'TwoDogs1299';
+	$tatico->app = 'E8963A17-7967-4A4D-BDA9-12B1F9551F06';
+	$tatico->token = "Basic " . base64_encode("$tatico->usuario:$tatico->senha");
+	$tatico->IDUsr = '143';
+	$tatico->Usr = ' 98c53f88-cd6e-4108-8521-1a6fef45c47f';
+	$tatico->Hash = 'hElKRkJ+QEc6R0pGRzpEfkSCOoCCfko6QEGAQIVKRUVAhT5GSkmBRUKFSUk6gYJGhDpEQD5JOklFQUA6QH5GhYSFREWBREeFl5uRgpGGlkBBSkpAREI=';
+	// echo $tatico->token;
+
+	$bling = new StdClass();
+	// $bling->token = 'e0f28e22be572bec99f551f8b9324f8f163d49471b36727236fda6a4a81404c6a548973c';
+	$bling->token = '44d23f8acdb04dce6293af6bb417c8bb9654c666134fc59b7682fd4ae16d870a2095dcfd';
+	$bling->format = 'json';
+	$bling->loja = '203427008';
